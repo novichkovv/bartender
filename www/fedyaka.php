fe4d090a11317eb3592c7af5565d88f17a26cfe4
Novichkov <novichvkovv@bk.ru>
2015-07-30 15:51:41 +0300

    No Message
    Commit with no message
    
    Signed-off-by: Novichkov <novichvkovv@bk.ru>

--- www/fedyaka.php
+++ www/fedyaka.php
@@ -10,14 +10,14 @@
     <form method="post" action="">
         <?php for($i = 1; $i < 9; $i ++): ?>
             <label>x<?php echo $i; ?></label>
-            <input style="width: 10px;" name="x[<?php echo $i; ?>]" value="<?php echo rand(1,3); ?>" type="text">
+            <input style="width: 10px;" name="x[<?php echo $i; ?>]" value="<?php echo 1;//rand(1,3); ?>" type="text">
         <?php endfor; ?>
         <input type="submit" name="show">
     </form>
     <div style="width: 100%;">
         <?php if(isset($_POST['show'])): ?>
             <?php foreach($_POST['x'] as $k => $width): ?>
-                <div style="margin: 1%; float: left; height: 100px; background-color: #63adc5; width: <?php echo (10 * $width - 1*$width); ?>%">
+                <div style="margin: 1%; float: left; height: 100px; background-color: #63adc5; width: <?php echo (10 * $width - $width*2); ?>%">
                     <h1 style="text-align: center; margin-top: 20px;"><?php echo $k; ?></h1>
                 </div>
             <?php endforeach; ?>
